bd48b3334a8096961022dac9f9bbc3e843eaca10
Ricardo Gayer <ricardo.r.gayer@gmail.com>
2022-06-13 13:07:58 -0300

    move font p/ assets

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -13,7 +13,7 @@ export const HeroesApp = () => {
 
   return (
     <div className="m-4">
-        <h1 className="font-inter text-transparent font-bold text-4xl bg-gradient-to-r from-fuchsia-500 to-indigo-500 w-fit bg-clip-text"> Heroes App </h1>
+        <h1 className="text-transparent font-bold text-4xl bg-gradient-to-r from-fuchsia-500 to-indigo-500 w-fit bg-clip-text"> Heroes App </h1>
         <h2 className="text-gray-500 text-md mt-1"> O site para amantes dos heróis de gibis</h2>
         <p className="text-gray-900 text-sm mt-4 mb-4 font-extralight italic"> Aqui você encontra as melhores histórias de gibi. Vonte a ser criança conosco. Assine agora e não perca a oportunidade! </p>
         <button className="w-32 border border-fuchsia-500 py-2 rounded-md text-fuchsia-500 hover:bg-fuchsia-50" onClick={handlerCanceler}> Cancelar </button>
